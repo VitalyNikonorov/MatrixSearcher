5d8d46b1d9d4c3d650e273861bb3fe29786fcab0
nik-vitaly <nik.vitaly@mail.ru>
2016-04-14 00:27:24 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+/**
+ * Created by vitaly on 14.04.16.
+ */
+public class Main {
+
+    private final static String FILE_NAME = "input.txt";
+
+    public static void main(String[] args){
+        String directory = System.getProperty("user.dir");
+        File inputFile = new File(String.format("%s/%s", directory, FILE_NAME));
+
+        
+
+    }
+
+}
